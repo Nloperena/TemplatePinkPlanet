43b26b288491f29d3d2f60aad81f48fffdbb716f
Nicholas Loperena <nicholasloperena@gmail.com>
2020-01-02 16:20:14 -0500

    added in styles

---
+++ assets/JavaScript/app.js
@@ -0,0 +1,13 @@
+
+  $(document).ready(function(){
+    
+    $('.fixed-action-btn').floatingActionButton();
+    $('.sidenav').sidenav();
+    $('.carousel.carousel-slider').carousel({
+      fullWidth: true,
+      indicators: true
+    });
+    $('.modal').modal();
+
+    // alert('Hi, this website is just a template and ANY change in style could be easily implemented for you. Have a fun look!')
+  });
